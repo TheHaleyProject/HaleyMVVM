12a927b07879d7da137ef466bf23909ba95847a5
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2021-03-11 13:30:50 +0400

    changes to assembly info

---
+++ HaleyMVVM/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Markup;
+using System;
+
+
+[assembly: XmlnsPrefix("http://schemas.hpod9.com/haley/mvvm", "hly")]
+//FOR XAML NAMESPACES - MVVM
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/mvvm", "Haley.Enums")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/mvvm", "Haley.MVVM.Converters")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/mvvm", "Haley.Abstractions")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/mvvm", "Haley.Models")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/mvvm", "Haley.MVVM")]
+
+//FOR XAML NAMESPACES - WPF
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/wpf", "Haley.WPF.ViewModels")]
+[assembly: XmlnsDefinition("http://schemas.hpod9.com/haley/wpf", "Haley.WPF.Views")]
