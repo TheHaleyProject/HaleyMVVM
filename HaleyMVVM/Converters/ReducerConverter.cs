ebfb401d25c49e1a3ed6f62fea6fa51fdf2b435b
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2022-06-29 14:01:56 +0400

    fix reducer converter

--- HaleyMVVM/Converters/ReducerConverter.cs
+++ HaleyMVVM/Converters/ReducerConverter.cs
@@ -15,9 +15,25 @@ namespace Haley.MVVM.Converters
         {
             try
             {
-                double _actual_length = (double)value;
-                if (parameter == null) return value; 
-                double _reducer = (double)parameter;
+                //Get direct value.
+                if (value == null) return value;
+                double _actual_length = 0.0;
+                if (value is double dblInput) {
+                    _actual_length = dblInput;
+                }
+                else {
+                    if (!double.TryParse(value.ToString(), out _actual_length)) return value;
+                }
+
+                //Get parameter
+                if (parameter == null) return value;
+                double _reducer = 0.0;
+                if (parameter is double dblParam) {
+                    _reducer = dblParam;
+                }
+                else {
+                    if (!double.TryParse(parameter.ToString(), out _reducer)) return value;
+                }
                 if (_reducer > _actual_length || _reducer == _actual_length) return value; 
                 return (_actual_length - _reducer);
             }
@@ -31,10 +47,22 @@ namespace Haley.MVVM.Converters
         {
             try
             {
-                double _converted_length = (double)value;
+                double _converted_length = (double)value; //We know that the value is double for sure.
+
+                //Get parameter
                 if (parameter == null) return value;
-                double _reducer = (double)parameter;
-                return (_converted_length + _reducer);
+                double _reducer = 0.0;
+                if (parameter is double dblParam) {
+                    _reducer = dblParam;
+                }
+                else {
+                    if (!double.TryParse(parameter.ToString(), out _reducer)) return value;
+                }
+
+                var _actual_length = _converted_length + _reducer;
+                if (_actual_length >= (2*_reducer) ) return _converted_length;
+
+                return _actual_length;
             }
             catch (Exception) //In case of any exception return the actual input value
             {
